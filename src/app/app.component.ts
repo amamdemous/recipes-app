64f4e83ca4b60e5032f15220550faa319f50dc09
Telman Babayev <babayevtolman@gmail.com>
2022-08-01 13:50:39 +0400

    Navigation implemented without angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  route: string = 'recipes';
+
+  handleNav(route: string) {
+    this.route = route;
+    console.log(route);
+  }
 }
