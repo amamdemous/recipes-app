c9722e4336b17974ee200cf06c1858cc03f0a627
Telman Babayev <babayevtolman@gmail.com>
2022-08-26 12:26:45 +0400

    Implement collapsing app header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,6 +16,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private userSub: Subscription;
   isAuthenticated = false;
+  isOpen = false;
 
   ngOnInit(): void {
     this.userSub = this.authService.user.subscribe((user) => {
